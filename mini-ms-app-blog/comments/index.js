7f84d1a58d5194aa7c7aaf6374b1251c4e0fd82e
Arnie Hsieh <tlhsieh0610@gmail.com>
2023-11-27 18:10:21 +0800

    mini-app add reveive-emmit event interface

--- mini-ms-app-blog/comments/index.js
+++ mini-ms-app-blog/comments/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const { randomBytes } = require("crypto");
 const cors = require("cors");
+const axios = require("axios");
 
 const app = express();
 app.use(bodyParser.json());
@@ -14,7 +15,12 @@ app.get("/posts/:id/comments", (req, res) => {
   res.send(commentsByPostId[req.params.id] || []);
 });
 
-app.post("/posts/:id/comments", (req, res) => {
+app.post("/events", (req, res) => {
+  console.log("Event received", req.body.type);
+  res.send({});
+});
+
+app.post("/posts/:id/comments", async (req, res) => {
   const id = randomBytes(4).toString("hex");
   const postId = req.params.id;
   //a single comment sent from users
@@ -23,6 +29,13 @@ app.post("/posts/:id/comments", (req, res) => {
   const comments = commentsByPostId[postId] || [];
   comments.push({ id, content });
   commentsByPostId[postId] = comments;
+
+  //emmit to eventbus
+  await axios.post("http://localhost:4005/events", {
+    type: "CommentCreated",
+    data: { id, title, postId },
+  });
+
   res.status(201).send(comments);
 });
 
