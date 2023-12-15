76272c6dd266cfcfba1138bf3b14d8a2d1147013
Arnie Hsieh <tlhsieh0610@gmail.com>
2023-12-15 20:44:17 +0800

    add ticketing currentUser/signout logic

--- ticketing/auth/src/routes/current-user.ts
+++ ticketing/auth/src/routes/current-user.ts
@@ -1,9 +1,15 @@
 import express from "express";
+import jwt from "jsonwebtoken";
+import { currentUser } from "../middlewares/current-user";
 
 const router = express.Router();
 
-router.get("/api/users/currentuser", (req, res) => {
-  res.send({});
+router.get("/api/users/currentuser", currentUser, (req, res) => {
+  if (!req.session?.jwt) {
+    return res.send({ currentUser: null });
+  }
+
+  res.send({ currentUser: req.currentUser || null });
 });
 
 export { router as currentUserRouter };
