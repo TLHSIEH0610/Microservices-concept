599f4af7fe4190341d1150ff76f922854450d06b
Arnie Hsieh <tlhsieh0610@gmail.com>
2023-12-17 19:51:02 +0800

    ticketing extract app from index

---
+++ ticketing/auth/src/app.ts
@@ -0,0 +1,31 @@
+import express from "express";
+import { json } from "body-parser";
+import { currentUserRouter } from "./routes/current-user";
+import { signinRouter } from "./routes/signin";
+import { signoutRouter } from "./routes/signout";
+import { signupRouter } from "./routes/signup";
+import { errorHandler } from "./middlewares/error-handler";
+import { NotFoundError } from "./errors/not-found-error";
+import cookieSession from "cookie-session";
+
+const app = express();
+app.set("trust proxy", true); //make sure that Express is aware that it's behind a proxy of Ingress-Nginx
+app.use(json());
+app.use(
+  cookieSession({
+    signed: false, // do not encrypt.
+    secure: true, //https only
+  })
+);
+app.use(currentUserRouter);
+app.use(signinRouter);
+app.use(signoutRouter);
+app.use(signupRouter);
+
+app.all("*", () => {
+  throw new NotFoundError();
+});
+
+app.use(errorHandler);
+
+export { app };
--- ticketing/auth/src/index.ts
+++ ticketing/auth/src/index.ts
@@ -1,33 +1,5 @@
-import express from "express";
-import { json } from "body-parser";
-import { currentUserRouter } from "./routes/current-user";
-import { signinRouter } from "./routes/signin";
-import { signoutRouter } from "./routes/signout";
-import { signupRouter } from "./routes/signup";
-import { errorHandler } from "./middlewares/error-handler";
-import { NotFoundError } from "./errors/not-found-error";
 import mongoose from "mongoose";
-import cookieSession from "cookie-session";
-
-const app = express();
-app.set("trust proxy", true); //make sure that Express is aware that it's behind a proxy of Ingress-Nginx
-app.use(json());
-app.use(
-  cookieSession({
-    signed: false, // do not encrypt.
-    secure: true, //https only
-  })
-);
-app.use(currentUserRouter);
-app.use(signinRouter);
-app.use(signoutRouter);
-app.use(signupRouter);
-
-app.get("*", () => {
-  throw new NotFoundError();
-});
-
-app.use(errorHandler);
+import { app } from "./app";
 
 const startup = async () => {
   try {
