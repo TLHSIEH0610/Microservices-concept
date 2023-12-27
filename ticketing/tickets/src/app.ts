221be4a3e2df1acd916c1d55c2d58ecb338a974e
Arnie Hsieh <tlhsieh0610@gmail.com>
2023-12-27 17:13:35 +0800

    add tickets model

--- ticketing/tickets/src/app.ts
+++ ticketing/tickets/src/app.ts
@@ -1,9 +1,8 @@
 import express from "express";
 import { json } from "body-parser";
-
-import { errorHandler, NotFoundError } from "@arnietickets/common";
-
+import { errorHandler, NotFoundError, currentUser } from "@arnietickets/common";
 import cookieSession from "cookie-session";
+import { createTicketRouter } from "../routes/new";
 
 const app = express();
 app.set("trust proxy", true); //make sure that Express is aware that it's behind a proxy of Ingress-Nginx
@@ -11,9 +10,11 @@ app.use(json());
 app.use(
   cookieSession({
     signed: false, // do not encrypt.
-    secure: true, //https only
+    secure: process.env.NODE_ENV !== "test", //https only
   })
 );
+app.use(currentUser);
+app.use(createTicketRouter);
 
 app.all("*", () => {
   throw new NotFoundError();
---
+++ ticketing/tickets/src/models/tickets.ts
@@ -0,0 +1,46 @@
+import mongoose from "mongoose";
+
+interface TicketAttrs {
+  title: string;
+  number: number;
+  userId: string;
+}
+
+interface TicketModel extends mongoose.Model<TicketDoc> {
+  build(attrs: TicketAttrs): TicketDoc;
+}
+
+interface TicketDoc extends mongoose.Document {
+  title: string;
+  number: number;
+  userId: string;
+}
+
+const ticketSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      require: true,
+    },
+    price: {
+      type: Number,
+      require: true,
+    },
+    userId: {
+      type: String,
+      require: true,
+    },
+  },
+  {
+    toJSON: {
+      transform(doc, ret) {
+        (ret.id = ret._id), delete ret.id;
+      },
+    },
+  }
+);
+
+ticketSchema.statics.build = (attrs: TicketAttrs) => new Ticket(attrs);
+const Ticket = mongoose.model<TicketDoc, TicketModel>("Ticket", ticketSchema);
+
+export { Ticket };
