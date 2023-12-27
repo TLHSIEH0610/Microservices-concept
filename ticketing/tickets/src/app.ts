82d09239db8c12fe82a2bc717b59b443fe37d912
Arnie Hsieh <tlhsieh0610@gmail.com>
2023-12-27 16:43:16 +0800

    fix tickets error and add mongo to skaffold

--- ticketing/tickets/src/app.ts
+++ ticketing/tickets/src/app.ts
@@ -1,9 +1,6 @@
 import express from "express";
 import { json } from "body-parser";
-import { currentUserRouter } from "./routes/current-user";
-import { signinRouter } from "./routes/signin";
-import { signoutRouter } from "./routes/signout";
-import { signupRouter } from "./routes/signup";
+
 import { errorHandler, NotFoundError } from "@arnietickets/common";
 
 import cookieSession from "cookie-session";
@@ -17,10 +14,6 @@ app.use(
     secure: true, //https only
   })
 );
-app.use(currentUserRouter);
-app.use(signinRouter);
-app.use(signoutRouter);
-app.use(signupRouter);
 
 app.all("*", () => {
   throw new NotFoundError();
--- ticketing/tickets/src/index.ts
+++ ticketing/tickets/src/index.ts
@@ -7,7 +7,10 @@ const startup = async () => {
     if (!process.env.JWT_KEY) {
       throw new Error("JWT_KEY must be defined!");
     }
-    await mongoose.connect("mongodb://auth-mongo-srv:27017/auth");
+    if (!process.env.MONGO_URI) {
+      throw new Error("MONGO_URI must be defined!");
+    }
+    await mongoose.connect(process.env.MONGO_URI);
     console.log("DB connected");
   } catch (e) {
     console.error(e);
