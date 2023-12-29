0eba239300456fe4cd29b473c345b323dc886f70
Arnie Hsieh <tlhsieh0610@gmail.com>
2023-12-29 12:49:18 +0800

    add tickets put method

--- ticketing/tickets/src/app.ts
+++ ticketing/tickets/src/app.ts
@@ -5,6 +5,7 @@ import cookieSession from "cookie-session";
 import { createTicketRouter } from "../routes/new";
 import { showTicketRouter } from "../routes/show";
 import { indexTicketRouter } from "../routes";
+import { updateTicketRouter } from "../routes/update";
 
 const app = express();
 app.set("trust proxy", true); //make sure that Express is aware that it's behind a proxy of Ingress-Nginx
@@ -19,6 +20,7 @@ app.use(currentUser);
 app.use(createTicketRouter);
 app.use(showTicketRouter);
 app.use(indexTicketRouter);
+app.use(updateTicketRouter);
 
 app.all("*", () => {
   throw new NotFoundError();
