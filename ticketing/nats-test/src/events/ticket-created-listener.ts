d060de675d9cb082ad6dfac2bfc659feadd6b955
Arnie Hsieh <tlhsieh0610@gmail.com>
2024-01-01 10:30:31 +0800

    add pub/sub type check

--- ticketing/nats-test/src/events/ticket-created-listener.ts
+++ ticketing/nats-test/src/events/ticket-created-listener.ts
@@ -1,10 +1,12 @@
 import { Message } from "node-nats-streaming";
 import { Listener } from "./base-listener";
+import { Subject } from "./subject";
+import { TicketCreatedEvent } from "./ticket-created-event";
 
-export class TicketCreatedListener extends Listener {
-  subject = "ticket:created";
+export class TicketCreatedListener extends Listener<TicketCreatedEvent> {
+  subject: Subject.TicketCreated = Subject.TicketCreated;
   queueGroupName = "payment-service";
-  onMessage(data: any, msg: Message) {
+  onMessage(data: TicketCreatedEvent["data"], msg: Message) {
     console.log("Event Data", data);
     msg.ack(); //tell NATS the message and has been processed(for manual ack).
   }
