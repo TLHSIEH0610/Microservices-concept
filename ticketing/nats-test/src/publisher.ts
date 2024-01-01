f5832418d7e9dd26dd699cd2dae090e346b35f1c
Arnie Hsieh <tlhsieh0610@gmail.com>
2024-01-01 10:39:59 +0800

    promisefy publisher

--- ticketing/nats-test/src/publisher.ts
+++ ticketing/nats-test/src/publisher.ts
@@ -1,20 +1,33 @@
 import nats from "node-nats-streaming";
+import { TicketCreatedPublisher } from "./events/ticket-created-publisher";
 
 //port forward(for test): kubectl port-forward pod's_name 4222:4222
 const stan = nats.connect("ticketing", "abc", {
   url: "http://localhost:4222",
 });
 
-stan.on("connect", () => {
+stan.on("connect", async () => {
   console.log("publisher connected to NATS");
 
-  const data = JSON.stringify({
-    id: "123",
-    title: "concert",
-    price: 40,
-  });
+  const publisher = new TicketCreatedPublisher(stan);
 
-  stan.publish("ticket:created", data, () => {
-    console.log("event published");
-  });
+  try {
+    await publisher.publish({
+      id: "123",
+      title: "concert",
+      price: 20,
+    });
+  } catch (err) {
+    console.error(err);
+  }
+
+  //   const data = JSON.stringify({
+  //     id: "123",
+  //     title: "concert",
+  //     price: 40,
+  //   });
+
+  //   stan.publish("ticket:created", data, () => {
+  //     console.log("event published");
+  //   });
 });
